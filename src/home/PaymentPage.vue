175ffe69ddb94eb0b80aebc0bab06acbf0b3a31d
Anna Kim <annajinee@gmail.com>
2020-03-03 21:31:19 +0900

    진행중..

--- src/home/PaymentPage.vue
+++ src/home/PaymentPage.vue
@@ -3,18 +3,16 @@
 
             <div class="row justify-content-center p-5">
                 <div class="col-lg-8 text-center font-weight-bold">
-                    <img class="mt-5" src="img/portfolio/quantimg/bi_width.png" style="width: 50%"/>
+                    <img class="mt-1" src="img/portfolio/quantimg/bi_width.png" style="width: 45%"/>
                 <br/>
                     <h1 class="mt-4 d-inline-block"><btn class="mt-0 btn-warning btn-lg font-weight-bolder">프리미엄 회원 &nbsp;<i class="fas fa-comment-dollar"></i></btn></h1>
                     <h3 class="d-inline-block font-weight-bolder">&nbsp;서비스 런칭 기념 특가</h3>
-
-                    <h3 class="mt-5 text-warning"><strike>1년 3,600,000원</strike></h3>
-                    <a class="btn-warning btn-xl btn-toolbar text-xl-center"><h1 style="margin-left: 30%" class="font-weight-bolder">1년 1,440,000원</h1></a>
                 </div>
             </div>
+        <img src="img/portfolio/quantimg/payment_price.png" style="width: 100%"/>
 
         <div class="container text-center">
-            <h3 class="d-inline-block text-danger">매일 <h1 class="mt-3 font-weight-bold color-red d-inline-block">3,950원</h1>으로 최대 수익률을 기록하라!</h3>
+            <h2 class="d-inline-block text-danger">매일 <h1 class="mt-3 font-weight-bold color-red d-inline-block">3,950원</h1>으로 최대 수익률을 기록하라!</h2>
 
             <p class="text-dark">(1년 상품 할인가 기준)</p>
             <table class="table table-bordered text-center mt-5 font-weight-bold bg-light">
@@ -84,7 +82,7 @@
                         <p class="color-red m-0">-50%</p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="point">
                     <th scope="row"><h5 class="font-weight-bold">얼리버드 X 기간할인 최종 할인가</h5>
                         <hr class="-divide">
                         <h5 class="font-weight-bold">최종 할인율</h5>
@@ -127,5 +125,11 @@
 
     .color-navy {
         color: #0B3861;
+    }
+
+    tr.point {
+        text-align:center;
+        border:double 5px #DF0101;
+        background-color:pink;
     }
 </style>
